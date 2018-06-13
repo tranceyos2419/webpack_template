3d763e3f879538838ffeb2544c0da48685e779c1
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2018-06-13 11:21:08 +0900

    compile scss test

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,1 +1,7 @@
-export const time = new Date();
+const time = new Date();
+
+let multiplyES6 = (x, y) => {
+    return x * y
+};
+
+export {time, multiplyES6};
--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,37 @@ const path = require('path');
 
 module.exports = {
     entry: [
-        'babel-polyfill',
+        'babel-polyfill', //for compiling js files more strongly
         './src/js/main.js'
     ],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    mode: 'production',
     module: {
         rules: [{
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env', 'stage-0']
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/, //exclude js files under node_modules
+                use: {
+                    loader: 'babel-loader', //using babel-loader to compile js
+                    options: {
+                        presets: ['env', 'stage-0'] //how to compile js files
+                    }
                 }
             }
-        }]
+            // {
+            //     test: /\.scss$/,
+            //     use: [
+            //         "style-loader", // creates style nodes from JS strings
+            //         "css-loader", // translates CSS into CommonJS
+            //         "sass-loader" // compiles Sass to CSS
+            //     ],
+            //     include: [
+            //         path.resolve(__dirname, 'node_modules'),
+            //         path.resolve(__dirname, 'path/to/imported/file/dir')
+            //     ]
+            // }
+        ]
     }
 }
